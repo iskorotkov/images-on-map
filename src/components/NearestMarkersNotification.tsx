38eda77f25c8cfc25a632bfe29f2f9fbb9526ae1
Ivan Korotkov <korotkov.ivan.s@gmail.com>
2022-02-20 21:06:49 +0500

    feat(notifications): do not clear and redisplay notifications

--- src/components/NearestMarkersNotification.tsx
+++ src/components/NearestMarkersNotification.tsx
@@ -1,4 +1,4 @@
-import { dismissAllNotificationsAsync, scheduleNotificationAsync } from 'expo-notifications'
+import { dismissNotificationAsync, scheduleNotificationAsync } from 'expo-notifications'
 import React, { memo, useEffect, useMemo, useState } from 'react'
 import { useAppSelector } from '../store/hooks'
 import { selectMarkers } from '../store/reducers/markersReducer'
@@ -6,11 +6,13 @@ import { Coords } from '../models/coords'
 import { getCurrentPositionAsync, startLocationUpdatesAsync } from 'expo-location'
 import { getDistance } from 'geolib'
 import { defineTask, unregisterTaskAsync } from 'expo-task-manager'
+import { Marker } from '../models/marker'
 
 export const NearestMarkersNotification = memo(({ distance = 1000 }: { distance?: number }) => {
   const markers = useAppSelector(selectMarkers)
 
   const [location, setLocation] = useState<Coords | null>(null)
+  const [shownMarkers, setShownMarkers] = useState<Marker[]>([])
 
   const closeMarkers = useMemo(() => {
     if (!location) {
@@ -68,11 +70,16 @@ export const NearestMarkersNotification = memo(({ distance = 1000 }: { distance?
     }
 
     ;(async () => {
-      await dismissAllNotificationsAsync()
+      const markersToHide = shownMarkers.filter(n => closeMarkers.indexOf(n) === -1)
+      for (const marker of markersToHide) {
+        await dismissNotificationAsync(marker.id)
+      }
 
-      for (const marker of closeMarkers) {
+      const markersToShow = closeMarkers.filter(marker => shownMarkers.indexOf(marker) === -1)
+      for (const marker of markersToShow) {
         const locationStr = `(${marker.location.latitude.toFixed(4)}, ${marker.location.longitude.toFixed(4)})`
-        scheduleNotificationAsync({
+        await scheduleNotificationAsync({
+          identifier: marker.id,
           content: {
             title: `You are near marker ${marker.name}`,
             body: `${getDistance(marker.location, location)}m | ${marker.images.length} images | ${locationStr}`,
@@ -83,8 +90,10 @@ export const NearestMarkersNotification = memo(({ distance = 1000 }: { distance?
           }
         })
       }
+
+      setShownMarkers(closeMarkers)
     })()
-  }, [closeMarkers, location])
+  }, [closeMarkers, location, shownMarkers])
 
   return <></>
 })
